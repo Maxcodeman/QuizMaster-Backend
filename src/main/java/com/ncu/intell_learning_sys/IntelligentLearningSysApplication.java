8df8164d1cf78fdb4df5f8c6f8ca805128fd96fa
Maxcodeman <107112162+Maxcodeman@users.noreply.github.com>
2023-12-04 13:40:05 +0800

    实现项目的一些基本配置

---
+++ src/main/java/com/ncu/intell_learning_sys/IntelligentLearningSysApplication.java
@@ -0,0 +1,13 @@
+package com.ncu.intell_learning_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentLearningSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentLearningSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncu/intell_learning_sys/IntelligentLearningSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncu.intell_learning_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentLearningSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
