ff4f0ddfabe545d958f7b5794223d7ff83c5e3d2
IAmRoze <oasis0109@outlook.com>
2024-06-18 16:37:14 +0800

    更改用户端查询 根据用户ID检索出其名下所有错过的的所有题目的 数据库查询逻辑

--- src/main/java/com/ncu/quiz_master_backend/mapper/QuestionForUsersMapper.java
+++ src/main/java/com/ncu/quiz_master_backend/mapper/QuestionForUsersMapper.java
@@ -1,7 +1,6 @@
 package com.ncu.quiz_master_backend.mapper;
 
 import com.ncu.quiz_master_backend.entity.FavoriteQuestion;
-import com.ncu.quiz_master_backend.entity.Question;
 import com.ncu.quiz_master_backend.entity.QuestionForUsers;
 import com.ncu.quiz_master_backend.entity.WrongQuestion;
 import org.apache.ibatis.annotations.Insert;
@@ -59,22 +58,20 @@ public interface QuestionForUsersMapper {
             "where tb_favorite.user_id = #{userId} and tb_favorite.is_favorite = 1;")
     List<QuestionForUsers> getFavourByUserId(Integer userId);
 
-    @Select("select tb_wrong.question_id,\n" +
-            "       tb_question.question_desc,\n" +
-            "       tb_question.option_a,\n" +
-            "       tb_question.option_b,\n" +
-            "       tb_question.option_c,\n" +
-            "       tb_question.option_d,\n" +
-            "       tb_question.answer,\n" +
-            "       tb_question.category_id,\n" +
-            "       COALESCE(tb_wrong.wrong_count, 0) AS wrong_count,\n" +
-            "       tb_favorite.is_favorite,\n" +
-            "       tb_question.type\n" +
-            "from tb_user\n" +
-            "         Join tb_favorite on tb_user.user_id = tb_favorite.user_id\n" +
-            "         join tb_question on tb_favorite.question_id = tb_question.question_id\n" +
-            "         join tb_wrong on tb_question.question_id = tb_wrong.question_id\n" +
-            "        AND tb_favorite.question_id = tb_wrong.question_id\n" +
-            "where tb_wrong.user_id = #{userId};")
+    @Select("select tw.question_id,\n" +
+            "tq.question_desc,\n" +
+            "tq.option_a,\n" +
+            "tq.option_b,\n" +
+            "tq.option_c,\n" +
+            "tq.option_d,\n" +
+            "tq.answer,\n" +
+            "tq.category_id,\n" +
+            "COALESCE(tw.wrong_count, 0) AS wrong_count,\n" +
+            "COALESCE(tf.is_favorite, 0) as is_favorite,\n" +
+            "tq.type\n" +
+            "from tb_wrong tw\n" +
+            "join tb_question tq on tw.question_id = tq.question_id\n" +
+            "left join tb_favorite tf on tq.question_id = tf.question_id\n" +
+            "where tw.user_id = #{userId}")
     List<QuestionForUsers> getWrongByUserId(Integer userId);
 }
